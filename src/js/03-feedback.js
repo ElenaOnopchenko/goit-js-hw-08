65a17f22ef3cfaed9ffac5afa05ddfe3c62fec7a
ElenaOnopchenko <martiniquejoy@gmail.com>
2022-09-16 20:19:11 +0200

    add task 2 and 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const textarea = document.querySelector('textarea[name="message"]');
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    const formData = { email: email.value, textarea: textarea.value };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+const load = key => {
+  try {
+    const savedData = localStorage.getItem(key);
+    return savedData === null ? undefined : JSON.parse(savedData);
+  } catch (error) {}
+};
+
+const loadData = load(STORAGE_KEY);
+if (loadData) {
+  email.value = loadData.email;
+  textarea.value = loadData.textarea;
+}
