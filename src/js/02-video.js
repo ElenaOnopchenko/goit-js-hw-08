efa7311a334a8d5c08a3043b3664492a0729fb44
ElenaOnopchenko <martiniquejoy@gmail.com>
2022-09-18 21:36:39 +0200

    updates tasks 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,14 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(STORAGE_KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
+
 player.on(
   'timeupdate',
-  throttle(e => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(e.seconds));
+  throttle(({ seconds }) => {
+    localStorage.setItem(STORAGE_KEY, seconds);
   }, 1000)
 );
-
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,18 +3,19 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
-const textarea = document.querySelector('textarea[name="message"]');
+const message = document.querySelector('textarea[name="message"]');
 
 form.addEventListener(
   'input',
   throttle(e => {
-    const formData = { email: email.value, textarea: textarea.value };
+    const formData = { email: email.value, message: message.value };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
   }, 500)
 );
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  console.log({ email: email.value, message: message.value });
   form.reset();
   localStorage.removeItem(STORAGE_KEY);
 });
@@ -23,11 +24,13 @@ const load = key => {
   try {
     const savedData = localStorage.getItem(key);
     return savedData === null ? undefined : JSON.parse(savedData);
-  } catch (error) {}
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
 };
 
 const loadData = load(STORAGE_KEY);
 if (loadData) {
   email.value = loadData.email;
-  textarea.value = loadData.textarea;
+  message.value = loadData.message;
 }
